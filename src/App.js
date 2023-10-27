031934bf6d64902e77aabc11d25081cfbd5c2390
pr19gya <pragya.shri2001@gmail.com>
2023-10-27 13:12:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import { useState } from 'react';
+import Textform from './components/Textform';
+import Alerts from './components/Alerts';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  const [mode, setMode]=useState('light');
+  const [alert, setAlert]=useState(null);
+
+  const showAlert=(message, type)=>{
+    setAlert({
+      msg: message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+  }
+
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='grey';
+      showAlert("Dark mode has been enabled","success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light mode has been enabled","success");
+    }
+  }
+  return (
+    <>
+    <Router>
+      <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alerts alert={alert}/>
+      <div className="container">
+      <Routes>
+      <Route path="/about" element={<About />} />
+          
+          {/* <Route path="/">
+            
+            <Textform showAlert={showAlert} heading="Enter your text to analyze"/> */}
+       <Route path="/" element={<Textform heading="Enter the text to analyze below" showAlert={showAlert}/>} />
+          {/* </Route> */}
+        </Routes>
+        </div>
+        </Router>
+      
+      {/* <About/> */}
+      
+      
+
+
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Textform.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react'
+
+export default function Textform(props) {
+    const handleUpClick=()=>{
+        // console.log("Uppercase was clicked" + text);
+        let newText=text.toUpperCase();
+
+        setText(newText);
+        props.showAlert("Converted to UpperCase", "success");
+    }
+    const handleLoClick=()=>{
+      // console.log("Lowercase was clicked" + text);
+      let newText=text.toLowerCase();
+
+      setText(newText);
+      props.showAlert("Converted to LowerCase", "success");
+  }
+
+  const handleClearClick=()=>{
+    // console.log("Lowercase was clicked" + text);
+    let newText='';
+
+    setText(newText);
+    props.showAlert("Text is Cleared", "success");
+}
+
+const speak = () => {
+  let msg = new SpeechSynthesisUtterance();
+  msg.text = text;
+  window.speechSynthesis.speak(msg);
+}
+
+    const handleOnChange=(event)=>{
+        console.log("on change");
+        setText(event.target.value);
+    }
+    const[text, setText]=useState('Enter Text Here');
+    //setText('New Text');
+
+    
+  return (
+
+
+   
+  <>
+  <div className="mb-3">
+    <label htmlFor="Text" style={{color : props.mode==='dark'?'white':'black'}} className="form-label my-3">{props.heading} </label>
+    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor :props.mode==='dark'?'grey':'white'}} id="exampleInputPassword1" rows="9"/>
+  </div>
+  
+  <button type="submit" className="btn btn-primary mx-1" onClick={handleUpClick}>Convert To Uppercase</button>
+  <button type="submit" className="btn btn-primary mx-1" onClick={handleLoClick}>Convert To LowerCase</button>
+  <button type="submit" className="btn btn-primary mx-1" onClick={handleClearClick}>Clear</button>
+  <button type="submit" onClick={speak} className="btn btn-warning mx-1 my-2">Speak</button>
+  <h3 className="my-2" style={{color : props.mode==='dark'?'white':'black'}}>Your Text Summary</h3>
+  <div>{text.split(" ").length} words</div>
+  <div >{text.length} characters</div>
+  <div >{text.split(".").length-1} sentences</div>
+  
+
+  </>
+  )
+}
